b87783e0eaa9448d76a77a8e4db740e71c9fbd64
Cristhian Martínez <martinezcuestacris@gmail.com>
2023-11-03 20:19:13 -0500

    se avanza en el trabajo de baseDatos

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('BBDD - suicidios.csv')
+
+df.columns
+
+columna = df.loc[:,'nombre_comuna'].unique() 
+columna
+
+option = st.selectbox(
+    'Ingrese el genero',
+    columna)
+
+st.write('You selected:', option)
+
+df =df[df['nombre_comuna'] == option]
+
+df
+
+
